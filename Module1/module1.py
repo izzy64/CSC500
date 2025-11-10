e070a88e6c6e58f2a00dc1439088c6cb19e208f4
Israel Bentley <israel.bentley@icloud.com>
2025-11-10 13:12:19 -0700

    Module 1

---
+++ Module1/module1.py
@@ -0,0 +1,13 @@
+# Part 1
+num1 = int(input()) # Get user-input num1
+num2 = int(input()) # Get user-input num2
+print(f"The sum of {num1} and {num2} is: {num1+num2}") # print sum output
+print(f"The difference of {num1} and {num2} is: {num1-num2}") # print difference output
+
+# Part 2
+num1 = int(input()) # Get user-input num1
+num2 = int(input()) # Get user-input num2
+print(f"The product of {num1} and {num2} is: {num1*num2}") # print product output
+print(f"The quotient of {num1} and {num2} is: {num1/num2}") # print quotient output
+
+
