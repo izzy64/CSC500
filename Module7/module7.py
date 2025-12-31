b364ccac8eea82be01467565937db1c2441f14d5
Israel Bentley <israel.bentley@icloud.com>
2025-12-31 11:20:48 -0700

    module7

---
+++ Module7/module7.py
@@ -0,0 +1,28 @@
+room_number = {
+    "CSC101": "3004",
+    "CSC102": "4501",
+    "CSC103": "6755",
+    "NET110": "1244",
+    "COM241": "1411",
+}
+
+instructors = {
+    "CSC101": "Haynes",
+    "CSC102": "Alvarado",
+    "CSC103": "Rich",
+    "NET110": "Burke",
+    "COM241": "Lee",
+}
+
+meeting_time = {
+    "CSC101": "8:00 a.m.",
+    "CSC102": "9:00 a.m.",
+    "CSC103": "10:00 a.m.",
+    "NET110": "11:00 a.m.",
+    "COM241": "1:00 p.m.",
+}
+
+print("Enter a course number to get class information")
+course_num = str(input())
+
+print(f"Course {course_num} meets in {room_number[course_num]} at {meeting_time[course_num]} with Professor {instructors[course_num]}")
